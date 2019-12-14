128d5fb455dfebafe1b54febdb2452d1b8b3e821
lis <listmsz@gmail.com>
2019-12-14 14:26:17 +0100

    obsluga wiecej niz jednego polaczenia

--- sieci/projekt1/server/src/main/java/imageoptimizer/server/App.java
+++ sieci/projekt1/server/src/main/java/imageoptimizer/server/App.java
@@ -12,50 +12,54 @@ public class App {
   public final static int SOCKET_PORT = 5000;
 
   public static void main(String[] args) throws IOException {
-
     ServerSocket serverSocket = null;
     serverSocket = new ServerSocket(SOCKET_PORT);
     Socket socket = null;
-    socket = serverSocket.accept();
-    String API_KEY = "WLFTW3thm5c7xQrvsXjNmMFqM1bWmSGD";
-    Tinify.setKey(API_KEY);
 
-    DataInputStream dis = new DataInputStream(socket.getInputStream());
-    DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+    while (true) {
+      try {
+        socket = serverSocket.accept();
+        String API_KEY = "WLFTW3thm5c7xQrvsXjNmMFqM1bWmSGD";
+        Tinify.setKey(API_KEY);
 
-    String targetPath = "";
-    int count = dis.readInt();
-    byte[] bs = new byte[count];
-    dis.read(bs);
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
-    for (int i = 0; i < count; i++) {
-      char c = (char) bs[i];
-      targetPath += c;
+        String targetPath = "";
+        int count = dis.readInt();
+        byte[] bs = new byte[count];
+        dis.read(bs);
+
+        for (int i = 0; i < count; i++) {
+          char c = (char) bs[i];
+          targetPath += c;
+        }
+
+        System.out.println(targetPath);
+        targetPath = targetPath.substring(0, count - 1);
+
+        FileOutputStream fos = new FileOutputStream(targetPath);
+        DataOutputStream ftos = new DataOutputStream(fos);
+
+        byte[] buffer = new byte[count];
+        while (dis.read(buffer) > 0) {
+
+          ftos.write(buffer);
+
+        }
+
+        Source source = Tinify.fromFile(targetPath);
+        source.toFile(targetPath);
+
+        System.out.println("IMAGE SAVED SUCCESSFULLY");
+
+        dis.close();
+        dos.close();
+        ftos.close();
+        socket.close();
+      } catch (IOException e) {
+        System.out.println("I/O error: " + e);
+      }
     }
-
-    System.out.println(targetPath);
-    targetPath = targetPath.substring(0, count-1);
-
-    FileOutputStream fos = new FileOutputStream(targetPath);
-    DataOutputStream ftos = new DataOutputStream(fos);
-
-    byte[] buffer = new byte[count];
-    while (dis.read(buffer) > 0) {
-
-      ftos.write(buffer);
-
-    }
-
-    Source source = Tinify.fromFile(targetPath);
-    source.toFile(targetPath);
-
-    System.out.println("IMAGE SAVED SUCCESSFULLY");
-
-    dis.close();
-    dos.close();
-    ftos.close();
-
-    socket.close();
-    serverSocket.close();
   }
 }
