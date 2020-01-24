58cf2d1357064badf16a035069fdc5aab6ef6875
lis <listmsz@gmail.com>
2020-01-24 20:34:31 +0100

    ex6

--- haskell/ex6.hs
+++ haskell/ex6.hs
@@ -25,10 +25,42 @@ swapSecondTos x = [x!!0, x!!((length x)-2)] ++ (drop 2 $ take ((length x) - 4)  
 countElem a x = length $ filter (a==) x
 
 --Zadanie 4
-twoListsAreEqual x y = length x == length y && zipWith (==) x y
+twoListsAreEqual x y = (length x == length y) && (foldr (&&) True (zipWith (==) x y))
 
 --Zadanie 5
-twoListsHaveSameElems x y = length x == length y && filter (`elem` y) x
+twoListsHaveSameElems x y = length x == length y && (length $ filter (`elem` y) x) == length x
 
---Zadanie 6
+--Zadanie 6 --TODO
 -- sort x y = 
+
+
+--Zadanie 7
+-- data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+
+-- getNodeValue (Node a l r) = a
+-- getLeftNode (Node a l r) = l
+-- getRightNode (Node a l r) = r
+
+-- t =   
+--     Node 1  
+--         (Node 2  
+--             (Node 4 Empty Empty)  
+--             (Node 5 Empty (Node 8 Empty Empty)) 
+--         )  
+--         (Node 3  
+--             (Node 6 Empty (Node 9 Empty Empty))
+--             (Node 7 Empty Empty)
+--         )
+
+-- wat Empty Empty = True
+-- wat t1 Empty = False
+-- wat Empty t2 = False
+-- wat t1 t2 = (getNodeValue(t1) == getNodeValue(t2)) && (wat getLeftNode(t1) getLeftNode(t2)) && areTreeEqual(getRightNode(t1), getRightNode(t2))
+
+
+
+
+-- isSubtree Empty Empty = True
+-- isSubtree Empty t2 = True
+-- isSubtree t1 Empty = False
+-- isSubtree t1 t2 = areTreeEqual(t1, t2) || isSubtree(t1, getLeftNode(t2)) || isSubtree(t1, getRightNode(t2))
