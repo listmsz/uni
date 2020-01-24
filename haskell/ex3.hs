e41178a531259450b1b19cefd0e4c0880e3a355e
lis <listmsz@gmail.com>
2020-01-24 17:30:07 +0100

    haskell ex3

--- haskell/ex3.hs
+++ haskell/ex3.hs
@@ -1,4 +1,5 @@
 import Data.List
+import System.IO 
 
 --Zadanie 1
 squareElems l = map (\x -> x^2) l --squareElems [1,2,3] = [1,4,9]
@@ -24,9 +25,63 @@ getCarMarkByCountry a   | a == "Sweden" = Saab
 
 -- b
 getCarAvgSpeed :: Moto -> Int
-getCarAvgSpeed a = case a of
-                        Saab -> 240
-                        Mercedes -> 220
-                        Lotus -> 340
-                        Jeep -> 180
-                        Mazda -> 160
+getCarAvgSpeed a = case a of Saab -> 240
+                             Mercedes -> 220
+                             Lotus -> 340
+                             Jeep -> 180
+                             Mazda -> 160
+
+--Zadanie 3
+--a
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+t :: Tree Int
+t =   
+    Node 1  
+        (Node 2  
+            (Node 4 Empty Empty)  
+            (Node 5 Empty (Node 8 Empty Empty)) 
+        )  
+        (Node 3  
+            (Node 6 Empty (Node 9 Empty Empty))
+            (Node 7 Empty Empty)
+        )
+
+--b
+tCh :: Tree Char
+tCh =   
+    Node 'a' 
+        (Node 'b'  
+            Empty
+            (Node 'd' (Node 'f' Empty Empty) Empty) 
+        )  
+        (Node 'c' 
+            (Node 'e' Empty (Node 'g' Empty Empty))
+            Empty
+        )
+
+preorder:: Tree a -> [a]
+preorder Empty = []
+preorder (Node a l r) = [a] ++ preorder l ++ preorder r
+
+-- *Main> preorder t
+-- [1,2,4,5,8,3,6,9,7]
+-- *Main> preorder tCh
+-- "abdfceg"
+
+inorder:: Tree a -> [a]
+inorder Empty = []
+inorder (Node a l r) = inorder l ++ [a] ++ inorder r
+
+-- *Main> inorder t
+-- [4,2,5,8,1,6,9,3,7]
+-- *Main> inorder tCh
+-- "bfdaegc"
+
+postorder:: Tree a -> [a]
+postorder Empty = []
+postorder (Node a l r) = postorder l ++ postorder r ++ [a]
+
+-- *Main> postorder t
+-- [4,8,5,2,9,6,7,3,1]
+-- *Main> postorder tCh
+-- "fdbgeca"
